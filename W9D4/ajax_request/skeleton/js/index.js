d05400417e9fadb3951ea811940827b7604c1fd5
stewart morales <stewartmorales129@gmail.com>
2021-02-24 21:29:05 -0500

    ajax

---
+++ W9D4/ajax_request/skeleton/js/index.js
@@ -0,0 +1,15 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+$.ajax({
+  url:'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  type: 'GET',
+  succes: function(weatherData) {
+    console.log(weatherData);
+  }
+})
+
+
+// Add another console log here, outside your AJAX request
+
+console.log("hello from js console")
